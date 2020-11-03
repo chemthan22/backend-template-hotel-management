cb3b92ea9d7fdb1c97240d748ad145c8475e303b
chemthan <kms.vudang@gmail.com>
2020-11-04 03:19:41 +0700

    First commit on template

---
+++ src/config/db.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    DB_CONFIG:{
+        URI:"mongodb://localhost:27017/hotel_managament"
+    },
+    DB_OPTIONS:{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+} 
---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dbConfig = require('./config/db.config');
+const app = express();
+
+//const port = process.env.PORT || 3000;
+const port = 5000;
+app.listen((e) => {
+    if(e) console.log(e)
+    console.log(`Server is running on port : ${port} `)
+})
+
+mongoose.connect(dbConfig.DB_CONFIG.URI,dbConfig.DB_OPTIONS,(e) => {
+    if (e) console.log(e);
+    console.log("DB Connected Successfully");
+})
+
+/*Root route */
+app.get('/',(req,res) => {
+    res.send("Root")
+})
+
+app.get('/mystack',(req,res) => {
+    res.send("OK");
+})
+
+
+/**
+ * GET ROUTE
+ */
+
